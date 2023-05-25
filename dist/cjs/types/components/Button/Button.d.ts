ee9b039d0a6dea4157caa58d2d0cbf8459b9a88a
jawwadurrehman <91111174+jawwadurrehman@users.noreply.github.com>
2023-05-25 15:11:36 +0500

    1.0.0

--- dist/cjs/types/components/Button/Button.d.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-export interface ButtonProps {
-    variant: string;
-    children: React.ReactNode;
-}
-declare const Button: (props: ButtonProps) => React.JSX.Element;
-export default Button;
--- dist/esm/types/theme/theme.d.ts
+++
@@ -1,5 +0,0 @@
-import React from "react";
-export declare const ThemeProvider: (props: {
-    children: React.ReactNode;
-}) => React.JSX.Element;
-export declare const useTheme: () => any;
